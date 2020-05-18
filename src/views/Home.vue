c2ef2e888d4c4b1f5a9329ad8c4c3292b519fc02
Jessica Jacelyn <jessicajacelyn@gmail.com>
2020-05-18 22:15:40 +0800

    WIP welcome card

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,241 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <v-app-bar dense fixed elevation="0" color="white">
+      <v-toolbar-title class="toolbar-font"
+        >welcome to tpa1, addy!</v-toolbar-title
+      >
+    </v-app-bar>
+
+    <br />
+
+    <v-img src="../assets/home.png" :aspect-ratio="6 / 13">
+      <v-row align="end" class=" white--text pa-2 fill-height text-md-center">
+        <v-col>
+          <div class="card-font text-center">
+            messages from all of us :) - tpa1
+          </div>
+        </v-col>
+      </v-row>
+    </v-img>
+
+    <v-container class="grey lighten-5" grid-list-md fluid>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                joey
+              </p>
+              <div class="card-font">
+                WELCOME ADDY!! Huehue Glad to hear that you’re coming into our
+                LG! #worshipleader HAHA jokes aside but yes aside from gettin to
+                know you through the hopekids band, really wanna know you more
+                as the opportunity is presented !! Looking forward to have u in
+                our LG and gettin to know you more !! Can be rather scary to
+                transfer to a new campus and new environment but yass we are
+                here for u too!! -Joey
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                cheryl
+              </p>
+              <div class="card-font">
+                hello Addy!!! just wanted to welcome you to Penang Island... JK
+                but thank you for being so courageous to move from NYP to TP to
+                be a support to us! seeing how you serve and lead the people in
+                Penang heartens me to know that you'll do the same and will do
+                your best for His people and God! I cannot assure you that it
+                will be an easy road ahead, but know that we got your back and
+                we are willing to go through it together with you! jiayou Addy!
+                💪🏼 agape, cheryl
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400">
+            <v-parallax src="../assets/pic1.jpg" height="300"></v-parallax>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                kendra
+              </p>
+              <div class="card-font">
+                hello addy!! hope to get to know u moreeeee in the years to
+                come!!
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                kwok ming
+              </p>
+              <div class="card-font">
+                WELCOME TO OUR LG YO!!HOPE TO KNOW U BTR AND HOPE U CAN ADAPT
+                WELL INTO OUR CAMPUS!!! -km
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                isabelle
+              </p>
+              <div class="card-font">
+                Heyy Addy!! WELCOME TO TPA1!! Hope to get to know u more in yr
+                TP journey & can't wait to serve w u!:) Hope u feel welcomed &
+                connected to this LG! Though idk u yet, I hope that this LG can
+                be a spiritual fam to u & one that really points u back to
+                Christ & spurs u on in yr walk! Feel free to also suggest any
+                initiatives from NYP that u feel might be applicable to this
+                LG!! We r open & ready to grow to be more like Christ HAHAHA!
+                And yeah welcome once again & I hope u enjoy this community!:) -
+                Isabelle
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400">
+            <v-parallax src="../assets/pic2.jpg" height="300"></v-parallax>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                jianing
+              </p>
+              <div class="card-font">
+                Hii addy, Welcomee to TPA1!! Really glad to have u in this lg ,
+                hope to get to know u more and to also journey with u in christ
+                :)) -jianingg
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                miriam
+              </p>
+              <div class="card-font">
+                wadduupp addy!! cant believe that you've agreed to come over to
+                serve with us in the TP ministry!! i am truly looking forward to
+                working with you, and thank you for choosing to come also. im
+                sure the season of deliberation was tough, but i hope that it
+                drew you closer to God as well. exciting times are ahead, and
+                tho there might be bumps, rest assured that the leaders are here
+                to support you too :)) feel free to ask us any questions
+                alright, we got your back. welcome to tp!!! agape, miriam
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class="card-title">
+                sarah
+              </p>
+              <div class="card-font">
+                A big Welcome for u Addy! As a person just joined not long ago,
+                I believe u will have many fun with so many cute and genuine
+                people here. Yeah we got u! Let’s know each other more in the
+                time to come!🎉
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs12>
+          <v-card elevation="0" max-width="400" data-aos="zoom-out">
+            <v-card-text>
+              <p class=" card-title">
+                lucas
+              </p>
+              <div class="card-font">
+                Hey Addy, welcome to TPA1! Super excited to see the new
+                transfers flow in and you aren't an exception. I hope you had a
+                fun time in where ever you were before poly, but being in TP
+                will be one of the best years of your life. Looking forward to
+                doing life together with you and meeting you irl, we are all in
+                this together!
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  computed: {
+    messages() {
+      return [
+        {
+          id: 0,
+          name: "joey",
+          message:
+            "WELCOME ADDY!! Huehue Glad to hear that you’re coming into our LG! #worshipleader HAHA jokes aside but yes aside from gettin to know you through the hopekids band, really wanna know you more as the opportunity is presented !! Looking forward to have u in our LG and gettin to know you more !! Can be rather scary to transfer to a new campus and new environment but yass we are here for u too!! -Joey",
+        },
+        {
+          id: 1,
+          name: "joey",
+          message:
+            "WELCOME ADDY!! Huehue Glad to hear that you’re coming into our LG! #worshipleader HAHA jokes aside but yes aside from gettin to know you through the hopekids band, really wanna know you more as the opportunity is presented !! Looking forward to have u in our LG and gettin to know you more !! Can be rather scary to transfer to a new campus and new environment but yass we are here for u too!! -Joey",
+        },
+      ];
+    },
+  },
 };
 </script>
+
+<style scoped>
+.toolbar-font {
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: bold;
+}
+.card-font {
+  font-family: "Helvetica";
+  font-size: 12px;
+  text-align: justify;
+}
+.card-title {
+  font-family: "Helvetica", Arial, sans-serif;
+  font-weight: bold;
+  font-size: 30px;
+}
+.parallax {
+  width: 100% !important;
+  /* height: 100% !important; */
+}
+</style>
